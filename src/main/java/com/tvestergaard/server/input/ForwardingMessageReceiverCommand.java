3e55eb955da7362923d51c589bfba9a94ba7cf55
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-09-04 11:13:31 +0200

    forward to all

--- src/main/java/com/tvestergaard/server/input/ForwardingMessageReceiverCommand.java
+++ src/main/java/com/tvestergaard/server/input/ForwardingMessageReceiverCommand.java
@@ -4,8 +4,8 @@ import com.tvestergaard.server.GeneralChatException;
 import com.tvestergaard.server.User;
 import com.tvestergaard.server.UserRepository;
 import com.tvestergaard.server.output.MessageTransmitter;
+import com.tvestergaard.server.output.messages.Message;
 import com.tvestergaard.server.output.messages.Recipients;
-import com.tvestergaard.server.output.messages.TextMessage;
 import org.json.JSONObject;
 
 /**
@@ -57,9 +57,62 @@ public class ForwardingMessageReceiverCommand implements MessageReceiverCommand
     {
         try {
             String message = payload.getString("message");
-            transmitter.send(Recipients.toAllExcept(sender), new TextMessage(sender, message));
+            transmitter.send(Recipients.toAll(), new TextMessage(sender, message));
         } catch (Exception e) {
             transmitter.send(Recipients.toThese(sender), new GeneralChatException(e));
+        }
+    }
+
+    /**
+     * Represents a message containing a chat message.
+     */
+    public static class TextMessage implements Message
+    {
+
+        private final static String SENDER_ATTRIBUTE  = "sender";
+        private final static String MESSAGE_ATTRIBUTE = "message";
+
+        /**
+         * The client who sent the message.
+         */
+        private final User sender;
+
+        /**
+         * The body of the message.
+         */
+        private final String message;
+
+        /**
+         * Creates a new {@link TextMessage}.
+         *
+         * @param sender  The client who sent the message.
+         * @param message The body of the message.
+         */
+        public TextMessage(User sender, String message)
+        {
+            this.sender = sender;
+            this.message = message;
+        }
+
+        /**
+         * Returns the identifier of the message type.
+         *
+         * @return The identifier of the message type.
+         */
+        @Override public String getMessageType()
+        {
+            return "message";
+        }
+
+        /**
+         * Adds the payload of the message to the provided {@code JSONObject}.
+         *
+         * @param payload The {@code JSONObject} to add the message payload to.
+         */
+        @Override public void addJson(JSONObject payload)
+        {
+            payload.put(SENDER_ATTRIBUTE, sender.toJson());
+            payload.put(MESSAGE_ATTRIBUTE, message);
         }
     }
 }
